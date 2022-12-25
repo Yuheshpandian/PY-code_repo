ae2e58aedecfb6f10069df8862da8a77d992e1a6
Yuhesh pandian.P.J <121353576+Yuheshpandian@users.noreply.github.com>
2022-12-25 14:27:07 +0530

    Add files via upload
    This code helps to encrypt a message and decrypt a encrypted message

---
+++ secret_lang.py
@@ -0,0 +1,39 @@
+#this dict helps us to translate the normal message/coded message to coded message/normal message
+lang={"a":"z","b":"y","c":"x","d":"w","e":"v","f":"u","g":"t","h":"s","i":"r","j":"q","k":"p","l":"o","m":"n","n":"m","o":"l","p":"k","q":"j","r":"i","s":"h","t":"g","u":"f","v":"e","w":"d","x":"c","y":"b","z":"a"," ":";","'":"&",".":"#"}
+
+#func that encrypt's a message
+def code(text):
+    x=""
+    for val in text.lower():
+        try:
+            x+=lang[val]
+        except:
+            return "invalid message,cant able to code"
+    return x
+    
+#func that decrypt's a code    
+def decode(text):
+    x=""
+    for val in text.lower():
+        for key,value in lang.items():
+            if val==value:
+                x+=key     
+    return x
+
+    
+#uncomment the next comment to encrypt a message  
+          
+"""
+text=input("enter the sentence or message you need to code:")
+text=code(text)
+print(text)
+"""
+
+
+#uncomment the next comment to encrypt a message
+
+"""
+text=input("enter the sentence or message you need to decode:")
+text=decode(text)
+print(text)
+""" 
