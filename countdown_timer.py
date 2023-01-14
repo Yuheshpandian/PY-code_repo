f0ed53e339e02ece81f15037f580d8af051c8f06
Yuhesh pandian.P.J <121353576+Yuheshpandian@users.noreply.github.com>
2023-01-14 20:51:47 +0530

    Update countdown_timer.py

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,16 +1,21 @@
+# importing libraries
 import time
 import pyttsx3
 
+# initialising
 engine = pyttsx3.init()
 
+# the number to count
 duration = int(input('enter tne number that I need to count: '))
 
+# main loop 
 for n in range(duration):
     print(n+1, end = '\r')
     engine.say(str(n+1))
     engine.runAndWait()
     
-    
+    # time pause
     time.sleep(0.02)
+
 
 print('TIME OVER')
